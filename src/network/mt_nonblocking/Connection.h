bb0d041296bdb90f76c6c27acd848bd713f11a29
3145tttt <3145tttt002@mail.ru>
2021-11-02 23:04:03 +0300

    try#1

--- src/network/mt_nonblocking/Connection.h
+++ src/network/mt_nonblocking/Connection.h
@@ -5,18 +5,27 @@
 
 #include <sys/epoll.h>
 
+//HW
+#include "afina/Storage.h"
+#include "afina/execute/Command.h"
+#include "afina/logging/Service.h"
+#include "protocol/Parser.h"
+#include <deque>
+
 namespace Afina {
 namespace Network {
 namespace MTnonblock {
 
 class Connection {
 public:
-    Connection(int s) : _socket(s) {
+    Connection(int s, std::shared_ptr<spdlog::logger> logger, std::shared_ptr<Afina::Storage> storage, size_t max = 10) : _socket(s) {
         std::memset(&_event, 0, sizeof(struct epoll_event));
         _event.data.ptr = this;
+        _logger = logger;
+        max_size = max;
     }
 
-    inline bool isAlive() const { return true; }
+    bool isAlive = true;
 
     void Start();
 
@@ -32,6 +41,19 @@ private:
 
     int _socket;
     struct epoll_event _event;
+
+    std::shared_ptr <spdlog::logger> _logger;
+    std::shared_ptr <Afina::Storage> pStorage;
+    std::string argument_for_command;
+    std::unique_ptr <Execute::Command> command_to_execute;
+    std::size_t arg_remains = 0;
+    Protocol::Parser parser;
+
+    std::deque <std::string> out_queue;
+    size_t max_size;
+    size_t head_offset = 0;
+
+    std::mutex queue_push_mutex;
 };
 
 } // namespace MTnonblock
